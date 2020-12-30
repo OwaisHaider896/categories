1ad61bf4bed0cb79f1e9d38279e0d365284d37b5
Owais Naqvi <65233517+OwaisHaider896@users.noreply.github.com>
2020-12-30 14:43:58 +0500

    remove font-bold

--- src/App.js
+++ src/App.js
@@ -7,25 +7,23 @@ export default function App() {
   return (
     <div
       className={`${
-        hover || active ? "bg-pink-800" : "bg-white-200"
-      } w-64 shadow-lg rounded m-6 p-2 cursor-pointer `}
+        hover || active ? "bg-purple-600" : "bg-white-200"
+      } w-64 shadow-${
+        hover || active ? "2xl" : "lg"
+      } rounded m-6 p-2 cursor-pointer `}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
       onClick={() => setActive((prevState) => !prevState)}
     >
       <div className="flex">
-        <div className="w-16 bg-pink-600 rounded-full h-16"></div>
+        <div className="w-16 bg-purple-900 rounded-full h-16"></div>
         <div className="ml-4 p-2">
-          <h1
-            className={`font-bold ${
-              hover || active ? `text-white` : `text-black`
-            }`}
-          >
-            Beauty
+          <h1 className={`${hover || active ? `text-white` : `text-black`}`}>
+            Marketing
           </h1>
           <div className="flex items-center">
             <h1
-              className={`font-bold ${
+              className={`text-xl ${
                 hover || active ? "text-white" : "text-purple-800"
               }`}
             >
